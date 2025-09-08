15482834c78defd53e3e752a2ad187e965cc0e40
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-09-09 01:33:53 +0200

    Refactor LinkPlaylistDto validation for URLs

--- packages/api/src/modules/playlists/dto/link-playlist.dto.ts
+++ packages/api/src/modules/playlists/dto/link-playlist.dto.ts
@@ -4,12 +4,10 @@ export class LinkPlaylistDto {
   @IsIn(['M3U','XTREAM'])
   type!: 'M3U'|'XTREAM';
 
-  // M3U
   @ValidateIf(o => o.type === 'M3U')
-  @IsUrl({ require_tld: false }) // autorise IP locales
+  @IsUrl({ require_tld: false })
   url?: string;
 
-  // Xtream
   @ValidateIf(o => o.type === 'XTREAM')
   @IsUrl({ require_tld: false })
   baseUrl?: string;
