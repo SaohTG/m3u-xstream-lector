80c90dbf9ef8d76b641e653124603615cf5b99b6
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-09-08 10:35:23 +0200

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Vite va injecter VITE_API_URL comme une **chaîne JSON** (toujours quotée)
 export default defineConfig({
   plugins: [react()],
-  server: { host: true, port: 5173 }
+  server: { host: true, port: 5173 },
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || '')
+  }
 })
