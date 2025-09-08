4cc9d235216188aaa8be00dc66bf81a8e8ffcc6e
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-09-09 00:26:19 +0200

    Convert User entity to UsersModule with TypeORM

--- packages/api/src/modules/users/user.entity.ts
+++ packages/api/src/modules/users/user.entity.ts
@@ -1,10 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
 
-@Entity('users')
-export class User {
-  @PrimaryGeneratedColumn('uuid') id: string;
-  @Column({ unique: true }) email: string;
-  @Column() password_hash: string;
-  @Column({ type: 'timestamptz', nullable: true }) email_verified_at: Date | null;
-  @CreateDateColumn() created_at: Date;
-}
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersService],
+  exports: [UsersService, TypeOrmModule],
+})
+export class UsersModule {}
