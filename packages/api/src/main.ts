55ef23e75da3b0c6d7ef62eaf07779c59a096a22
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-09-09 00:22:26 +0200

    Update main.ts

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -3,12 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  app.enableCors({ origin: [/localhost/, /.*/], credentials: true });
+  app.enableCors({ origin: [/^http:\/\/.*:5173$/, /.*/], credentials: true });
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
