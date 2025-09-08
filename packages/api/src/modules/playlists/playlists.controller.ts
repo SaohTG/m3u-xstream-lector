d8794dc1f8f9d966a13a469f5ef542f7783cdd5b
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-09-09 01:12:34 +0200

    Add validation pipe to link playlist endpoint

--- packages/api/src/modules/playlists/playlists.controller.ts
+++ packages/api/src/modules/playlists/playlists.controller.ts
@@ -1,13 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PlaylistsService } from './playlists.service';
+import { LinkPlaylistDto } from './dto/link-playlist.dto';
 
 @Controller('playlists')
 export class PlaylistsController {
   constructor(private svc: PlaylistsService) {}
 
   @Post('link')
-  link(@Body() body: any) {
-    // body: { type: 'M3U'|'XTREAM', url? baseUrl? username? password? }
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  link(@Body() body: LinkPlaylistDto) {
     return this.svc.link(body);
   }
 }
