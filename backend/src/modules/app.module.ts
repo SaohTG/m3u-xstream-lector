b3cc5fc8a12e9661ac4822793503883efccca464
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-09-08 13:03:23 +0200

    Update app.module.ts

--- backend/src/modules/app.module.ts
+++ backend/src/modules/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { getDataSourceOptions } from '../ormconfig';
 import { AuthModule } from './auth/auth.module';
 import { PlaylistsModule } from './playlists/playlists.module';
 import { LibraryModule } from './library/library.module';
@@ -10,11 +9,18 @@ import { TmdbModule } from './tmdb/tmdb.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({ useFactory: getDataSourceOptions }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      // ✅ charge automatiquement les entités des modules forFeature()
+      autoLoadEntities: true,
+      // OK en dev (recréation/ajustement du schéma)
+      synchronize: true,
+    }),
     AuthModule,
     PlaylistsModule,
     LibraryModule,
-    TmdbModule
-  ]
+    TmdbModule,
+  ],
 })
 export class AppModule {}
