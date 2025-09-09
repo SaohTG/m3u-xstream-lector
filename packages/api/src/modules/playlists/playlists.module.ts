45f6150df03b1d998153a10c16f2ed1885eb6806
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-09-09 16:12:51 +0200

    Integrate TypeOrmModule for Playlist entity
    Added TypeOrmModule import and configured for Playlist entity.

--- packages/api/src/modules/playlists/playlists.module.ts
+++ packages/api/src/modules/playlists/playlists.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlaylistsController } from './playlists.controller';
 import { PlaylistsService } from './playlists.service';
+import { Playlist } from './playlist.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Playlist])],
   controllers: [PlaylistsController],
   providers: [PlaylistsService],
+  exports: [PlaylistsService], // ⬅️ important pour Vod/Live
 })
 export class PlaylistsModule {}
