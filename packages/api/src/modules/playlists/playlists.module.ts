49c2c362ae906f6407d7e22408c0417861b65bdb
SaohTG <76484218+SaohTG@users.noreply.github.com>
2025-09-10 01:28:50 +0200

    Reorder imports and update module configuration

--- packages/api/src/modules/playlists/playlists.module.ts
+++ packages/api/src/modules/playlists/playlists.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PlaylistsController } from './playlists.controller';
-import { PlaylistsService } from './playlists.service';
 import { Playlist } from './playlist.entity';
+import { PlaylistsService } from './playlists.service';
+import { PlaylistsController } from './playlists.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Playlist])],
-  controllers: [PlaylistsController],
   providers: [PlaylistsService],
-  exports: [PlaylistsService], // ⬅️ important pour Vod/Live
+  controllers: [PlaylistsController],
+  exports: [PlaylistsService],
 })
 export class PlaylistsModule {}
